367fe7ce71e33f7206f40e3a96b447107cc0a4b2
Shin Jihye <jihye41q8@gmail.com>
2024-07-24 03:49:14 +0900

    Add Matching the number of slides

--- pptx_conversion.py
+++ pptx_conversion.py
@@ -3,10 +3,9 @@ from pptx.enum.shapes import MSO_SHAPE_TYPE
 from PIL import Image
 import io
 
-
 def extract_text_and_images(ppt_file_path):
     presentation = Presentation(ppt_file_path)
-    text_content_by_slide = []
+    text_content_by_slide = [''] * len(presentation.slides)  # 슬라이드 수만큼 빈 문자열로 초기화
     images_by_slide = {}
 
     for slide_number, slide in enumerate(presentation.slides, start=1):
@@ -29,10 +28,7 @@ def extract_text_and_images(ppt_file_path):
             images_by_slide[f"Slide {slide_number}"] = images
         
         if slide_text:
-            text_content_by_slide.append('\n'.join(slide_text))
+            text_content_by_slide[slide_number - 1] = '\n'.join(slide_text)  # 슬라이드 인덱스에 텍스트 추가
 
-
-    
     return images_by_slide, text_content_by_slide
-
 
