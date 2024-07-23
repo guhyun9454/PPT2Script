71cfd21d55c14658d12df00ea895f795caef994c
Shin Jihye <jihye41q8@gmail.com>
2024-07-23 17:24:55 +0900

    Fix Function Arguments

--- pptx_conversion.py
+++ pptx_conversion.py
@@ -6,17 +6,11 @@ import os
 
 def extract_text_and_images(ppt_file_path):
     presentation = Presentation(ppt_file_path)
-    base_name = os.path.splitext(os.path.basename(ppt_file_path))[0]
-    text_result_dir = os.path.join(os.path.dirname(ppt_file_path), f'result_txt/{base_name}_text')
-    
-    # 텍스트 결과 디렉토리 생성
-    os.makedirs(text_result_dir, exist_ok=True)
-
-    text_content = []
+    text_content_by_slide = []
     images_by_slide = {}
 
     for slide_number, slide in enumerate(presentation.slides, start=1):
-        slide_text = [f'--- Slide {slide_number} ---']
+        slide_text = []
         images = []
 
         for shape in slide.shapes:
@@ -35,16 +29,10 @@ def extract_text_and_images(ppt_file_path):
             images_by_slide[f"Slide {slide_number}"] = images
         
         if slide_text:
-            text_content.append('\n'.join(slide_text))
+            text_content_by_slide.append('\n'.join(slide_text))
 
-    # 텍스트 파일로 결과 저장
-    text_result_file = os.path.join(text_result_dir, f'{base_name}.txt')
-    with open(text_result_file, 'w', encoding='utf-8') as file:
-        file.write('\n\n'.join(text_content))
+
     
-    print(f"Text extracted and saved to {text_result_file}")
-    return images_by_slide
+    return images_by_slide, text_content_by_slide
 
-# 예시 사용 방법 (실제 사용시 주석 해제)
-extract_text_and_images('./test.pptx')
 
