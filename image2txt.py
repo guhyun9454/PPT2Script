97fc20693990af582e6bf22cfd2dfce6d0cad9c9
Shin Jihye <jihye41q8@gmail.com>
2024-07-23 17:26:38 +0900

    Fix Function Arguments

--- image2txt.py
+++ image2txt.py
@@ -1,21 +1,15 @@
-
-from PIL import Image
 from transformers import AutoProcessor, AutoModelForVision2Seq
-import os
-from pptx_conversion import extract_text_and_images
-import re
 
-def process_images_to_texts(images_by_slide, output_dir, ppt_file_path):
+
+def process_images_to_texts(images_by_slide, text_content_by_slide):
     model = AutoModelForVision2Seq.from_pretrained("microsoft/kosmos-2-patch14-224")
     processor = AutoProcessor.from_pretrained("microsoft/kosmos-2-patch14-224")
-    base_name = os.path.splitext(os.path.basename(ppt_file_path))[0]
-    os.makedirs(output_dir, exist_ok=True)
-    final_text_file_path = os.path.join(output_dir, f'all_{base_name}.txt')
+    
 
-    all_texts = []
-
-    for slide, images in images_by_slide.items():
-        slide_texts = set()  # 중복을 방지하기 위해 set 사용
+    all_images_texts = []
+    
+    for slide_number, images in images_by_slide.items():
+        slide_texts = set()
 
         for index, image in enumerate(images):
             prompt = "<grounding> An image of"
@@ -32,20 +26,14 @@ def process_images_to_texts(images_by_slide, output_dir, ppt_file_path):
             generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
             caption, entities = processor.post_process_generation(generated_text)
 
-            # "background"가 포함된 문구 제외
             if "background" not in caption:
-                slide_texts.add(caption)  # 중복 제거를 위해 set에 추가
+                slide_texts.add(caption)
 
         if slide_texts:
-            formatted_texts = "\n\n".join(slide_texts)  # Set에서 중복 제거된 텍스트를 조합
-            all_texts.append(f"--- {slide} ---\n" + formatted_texts)
-    
-    with open(final_text_file_path, 'w', encoding='utf-8') as file:
-        file.write("\n\n".join(all_texts))
+            formatted_texts = "\n\n".join(slide_texts)
+            all_images_texts.append(f"{formatted_texts}")
+        else:
+            all_images_texts.append("")
 
-    print(f"Generated text saved in file: {final_text_file_path}")
+    return all_images_texts, text_content_by_slide
 
-# 이미지 추출 및 텍스트 처리 예시
-images_by_slide = extract_text_and_images('./test.pptx')
-ppt_file_path = "./test.pptx"
-process_images_to_texts(images_by_slide, './result_image2txt', ppt_file_path)
